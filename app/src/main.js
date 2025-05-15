92db19c62a2421130882813fc0bc221d292bcd32
ramity <lewisclbrown@gmail.com>
2025-05-15 17:38:41 -0500

    misc updates, reordering scripts, updated packages, fixed some small stuff in mainjs and removed ./ from index.html

--- app/src/main.js
+++ app/src/main.js
@@ -1,21 +1,38 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow() {
-    const win = new BrowserWindow({
+function createWindow()
+{
+    const window = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
-            nodeIntegration: true
+            // Keep Electron and renderer separate
+            contextIsolation: true,
+            // Disable Node.js integration in the renderer for security.
+            nodeIntegration: false,
+            // Use a preload script to configure bridges.
+            preload: path.join(__dirname, "preload.js")
         }
     });
 
-    win.loadFile(path.join(__dirname, '../public/index.html'));
+    window.loadFile(path.join(__dirname, '../public/index.html'));
+
+    if (process.env.NODE_ENV === 'development')
+    {
+        window.webContents.openDevTools();
+    }
 }
 
-app.whenReady().then(createWindow);
+// Create window after electron is initialized
+app.on('ready', () => {
+    createWindow();
+});
 
+// Gracefully handle window-all-closed event
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+    if (process.platform !== 'darwin')
+    {
+        app.quit();
+    }
 });
